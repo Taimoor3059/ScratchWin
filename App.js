7a3b69c444bec1d23e69c22e4a1b2a9f4b38dc56
TaimoorTahir <taimoorhtahir@gmail.com>
2020-01-30 11:19:15 -0600

    starting up the project

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+
+import {Button} from 'native-base';
+import { FontAwesome } from "@expo/vector-icons";
+
+var itemArray = new Array(25).fill("empty");
 
 export default function App() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomNumber = ""
+    };
+  }
+
+
+
+
+
+
+
   return (
     <View style={styles.container}>
       <Text>Scartch and Win </Text>
